884cbdc102507c6c24f515a48d1b2fbfdec038e0
Jorge <jorgediazok@gmail.com>
2020-05-24 23:03:33 -0300

    adding stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  users = ['ryan', 'joe', 'cameron', 'jhon', 'bruce'];
+  name: string = 'Ryan Ray';
+  age: number;
+  address: {
+    street: string;
+    city: string;
+    telephone: number;
+  };
+  hobbies: string[];
+
+  constructor() {
+    this.age = 35;
+    this.address = {
+      street: 'Cuba 2121',
+      city: 'Buenos Aires',
+      telephone: 239283,
+    };
+    hobbies: ['sex', 'drugs', 'rock and roll'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, HolaMundoComponent],
+  declarations: [AppComponent, HolaMundoComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
