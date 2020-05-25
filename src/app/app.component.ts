67afe1fc31807d4188823a337dc626fc32b9cd2d
Jorge <jorgediazok@gmail.com>
2020-05-25 02:27:02 -0300

    destroying the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,10 @@ import { Data } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name: string = 'John Carter';
-  age: number = 28;
-
   constructor(private dataService: DataService) {
     this.dataService.getData().subscribe((data) => {
-      console.log(data);
+      this.posts = data;
     });
-  }
-
-  users2: string[] = ['luan', 'ivy', 'mia'];
-
-  deleteUser(usuario) {
-    for (let i = 0; i < this.users2.length; i++) {
-      if ((usuario = this.users2[i])) {
-        this.users2.splice(i, 1);
-      }
-    }
-  }
-
-  addUser(newUser) {
-    this.users2.push(newUser.value);
-    newUser.value('');
-    newUser.focus();
-    return false;
   }
 
   posts = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
-import { UserComponent } from './user/user.component';
-import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
 
+const routes: Route[] = [{ path: '', component: AppComponent }, {}, {}];
 @NgModule({
-  declarations: [AppComponent, HolaMundoComponent, UserComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, AboutComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(),
+  ],
   providers: [DataService],
   bootstrap: [AppComponent],
 })
